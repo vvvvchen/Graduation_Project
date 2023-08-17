8727ad8498ef3c131ecbe0592d24e93aff7e59f3
Valerie Chen <vchenin620@gmail.com>
2023-08-17 16:28:00 +0800

    Initial Commit

---
+++ Graduation_Project/Graduation_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graduation_ProjectApp.swift
+//  Graduation_Project
+//
+//  Created by Mac on 2023/8/17.
+//
+
+import SwiftUI
+
+@main
+struct Graduation_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
