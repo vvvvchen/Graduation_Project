7cc8585ea945f643d460b59c17ddcdb07d7a2349
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-08-18 17:47:56 +0800

    0818done

--- Graduation_Project/LoginView.swift
+++ Graduation_Project/LoginView.swift
@@ -7,7 +7,7 @@
 import SwiftUI
 
 struct LoginView: View
-{
+    {
     @State private var goTo: Bool=false
     @State private var account: String=""
     @State private var password: String=""
@@ -21,32 +21,27 @@ struct LoginView: View
     {
         NavigationStack
         {
-            VStack(spacing:20)
+            VStack(spacing:60)
             {
                 Circle()
                     .foregroundColor(.gray)
                     .frame(width: 150,height: 150)
+                    .padding(.top,50)
                 
                 VStack(spacing: 20)
                 {
-                    HStack
-                    {
-                        TextField("Account", text: $account)
-                            .padding()
-                            .frame(width: 300, height: 50)
-                            .background(Color.gray.opacity(0.1))
-                            .cornerRadius(100)
-                            .padding(10)
+                    TextField("Account", text: $account)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(100)
+                        .padding(10)
                         
-                    }
-                    HStack
-                    {
-                        SecureField("Password", text: $password)
-                            .padding()
-                            .frame(width: 300, height: 50)
-                            .background(Color.gray.opacity(0.1))
-                            .cornerRadius(100)
-                    }
+                    SecureField("Password", text: $password)
+                        .padding(10)
+                        .frame(width: 300, height: 50)
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(100)
                     
                     NavigationLink(destination: SigninView())
                     {
@@ -57,27 +52,39 @@ struct LoginView: View
                     NavigationLink(destination: CheckView(account: self.account, password: self.password))
                     {
                         Text("登入")
-                            .frame(width: 250, height: 25)
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .frame(width: 300, height: 50)
+                            .background(Color(red: 0.828, green: 0.249, blue: 0.115))
                             .cornerRadius(100)
                             .font(.title3)
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .background(.black)
-                            .cornerRadius(10)
+
                     }
-                    
-                    Spacer()
                 }
+                    HStack(spacing: 20)
+                    {
+                        Spacer()
+                        Circle()
+                            .foregroundColor(Color.gray.opacity(0.5))
+                            .frame(width: 50,height: 50)
+                        Spacer()
+                        Circle()
+                            .foregroundColor(Color.gray.opacity(0.5))
+                            .frame(width: 50,height: 50)
+                        Spacer()
+                        Circle()
+                            .foregroundColor(Color.gray.opacity(0.5))
+                            .frame(width: 50,height: 50)
+                        Spacer()
+                    }
+                    Spacer()
+
                 
             }
-            HStack
-            {
-                //111423
-            }
-            .padding()
+            //.padding()
             .navigationTitle(Text("LOG IN"))
             .navigationBarTitleDisplayMode(.inline)
-            //.offset(x:0,y:-80)
+
             .onAppear
             {
                 self.account=""
--- Graduation_Project/SigninView.swift
+++ Graduation_Project/SigninView.swift
@@ -12,58 +12,53 @@ struct SigninView: View {
     @State private var password: String=""
     @State private var goTo : Bool=false
     
-    var body: some View {
-        NavigationStack{
-            ZStack{
-                
-                Circle()
-                    .scale(0.2)
-                    .foregroundColor(.gray.opacity(0.15))
-                
+    var body: some View
+    {
+        NavigationStack
+        {
+            Circle()
+                .foregroundColor(.gray)
+                .frame(width: 150,height: 150)
+                .padding()
                 //Image("our loge")
-                
+            VStack(spacing: 60)
+            {
                 VStack(spacing: 20)
                 {
-                    VStack(spacing: 20)
-                    {
-                        HStack
-                        {
-                            Text("帳號")
-                            TextField("輸入您的帳號", text: self.$account)
-                        }
-                        HStack
-                        {
-                            Text("密碼")
-                            TextField("輸入您的密碼", text: self.$password)
-                        }
-                    HStack
-                        {
-                            Text("密碼")
-                            TextField("再次輸入密碼", text: self.$password)
-                                
-                        }
-                        .textFieldStyle(.roundedBorder)
-                        .font(.title)
-                    }
+                    TextField("輸入您的帳號", text: self.$account)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .frame(width: 300, height: 50)
+                        .cornerRadius(100)
+                    TextField("輸入您的密碼", text: self.$password)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .frame(width: 300, height: 50)
+                        .cornerRadius(100)
+                    
+                    TextField("再次輸入密碼", text: self.$password)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .frame(width: 300, height: 50)
+                        .cornerRadius(100)
+
+                }
                     NavigationLink(destination: LoginView())
                     {
-                        Text("請返往登入畫面，進行登入動作")
+                        Text("點選往返登入畫面，進行登入動作")
+                            .foregroundColor(Color(red: 0.574, green: 0.609, blue: 0.386))
                     }
-                    
                 }
-                
             }
-        }
-            
-        .padding()
-        .navigationTitle(Text("SIGN IN"))
-        .navigationBarTitleDisplayMode(.large)
-        //.onAppear/
+            .padding()
+            .navigationTitle(Text("SIGN IN"))
+            .navigationBarTitleDisplayMode(.inline)
+            //.onAppear/
     }
 }
-
 struct SigninView_Previews: PreviewProvider {
-    static var previews: some View {
-        SigninView()
+    static var previews: some View
+    {
+    SigninView()
     }
 }
