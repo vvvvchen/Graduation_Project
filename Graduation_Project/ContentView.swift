34ef46f2b5de58733442a0fff5835b57a40426bf
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-08-21 13:51:09 +0800

    0821 noon

--- Graduation_Project/ContentView.swift
+++ Graduation_Project/ContentView.swift
@@ -47,97 +47,108 @@ struct ContentView: View
                 .tag(2)
             }
             
-            SideView(showSide: self.$showSide)
-                .ignoresSafeArea(.all)
+            .overlay(
+                SideView(showSide: self.$showSide)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .ignoresSafeArea(.all)
+
+            )
+            // 在展開SideView時禁止使用TabView和HomeView
+            .disabled(showSide)
+
+            if showSide {
+                SideView(showSide: self.$showSide)
+                    .background(Color(.systemGray5).opacity(0.1))
+                    .onTapGesture {
+                        self.showSide = false
+                    }
+            }
         }
         .tint(.black)
         //不要顯示NavigationBarTitle
         .navigationBarTitleDisplayMode(.inline)
         //MARK: Toolbar
-
         .toolbar
         {
             //選單
-                ToolbarItem(placement: .navigationBarLeading)
-                {
-                    NavigationStack{
+            ToolbarItem(placement: .navigationBarLeading)
+            {
+                NavigationStack{
                     ZStack{
                         
-                    Button
-                    {
-                        self.showSide=true
-                    }
-                label:
-                    {
-                        VStack(spacing: 6)
+                        Button
                         {
-                            ForEach(0..<3)
-                            {_ in
-                                Capsule(style: .continuous)
-                                    .fill(.black)
-                                    .frame(width: 30, height: 3)
-                            }
+                            self.showSide=true
                         }
-                    }
-                    .opacity(self.select==1 ? 1:0)
-                }
-                    }
-                }
-                //搜尋列
-                ToolbarItem(placement: .principal)
-                {
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(Color(.systemGray5))
-                        .overlay
+                    label:
                         {
-                            HStack
+                            VStack(spacing: 6)
                             {
-                                Image(systemName: "magnifyingglass")
-                                
-                                Spacer()
+                                ForEach(0..<3)
+                                {_ in
+                                    Capsule(style: .continuous)
+                                        .fill(.black)
+                                        .frame(width: 30, height: 3)
+                                }
                             }
-                            .padding(.horizontal)
                         }
                         .opacity(self.select==1 ? 1:0)
-                }
-                
-                //照相
-                ToolbarItem(placement: .navigationBarTrailing)
-                {
-                    if(self.select==1)
-                    {
-                        Button
-                        {
-                            //照相功能
-                        }
-                    label:
-                        {
-                            Image(systemName: "camera.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: 30)
-                                .foregroundColor(.black)
-                        }
-                    }
-                    else if(self.select==0)
-                    {
-                        Button
-                        {
-                            //點擊「刪除紀錄」之後要執行的動作
-                        }
-                    label:
-                        {
-                            Text("刪除紀錄")
-                                .font(.body)
-                                .foregroundColor(.blue)
-                        }
                     }
                 }
             }
+            //搜尋列
+            ToolbarItem(placement: .principal)
+            {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(Color(.systemGray5))
+                    .overlay
+                {
+                    HStack
+                    {
+                        Image(systemName: "magnifyingglass")
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+                .opacity(self.select==1 ? 1:0)
+            }
             
+            //照相
+            ToolbarItem(placement: .navigationBarTrailing)
+            {
+                if(self.select==1)
+                {
+                    Button
+                    {
+                        //照相功能
+                    }
+                label:
+                    {
+                        Image(systemName: "camera.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 30)
+                            .foregroundColor(.black)
+                    }
+                }
+                else if(self.select==0)
+                {
+                    Button
+                    {
+                        //點擊「刪除紀錄」之後要執行的動作
+                    }
+                label:
+                    {
+                        Text("刪除紀錄")
+                            .font(.body)
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
         }
     }
-
+}
 
 struct SideView: View {
     @Binding var showSide: Bool
@@ -149,14 +160,15 @@ struct SideView: View {
                 Rectangle()
                     .fill(.green)
                     .frame(maxHeight: .infinity)
+                    //true點按不會收回
                     .onTapGesture {
-                        self.showSide=false
-                        
+                        self.showSide=true
                     }
             }
             .ignoresSafeArea(.all)
-            .offset(x:self.showSide ? geometry.size.width * -0.5 : -400)
+            .offset(x:self.showSide ? geometry.size.width * -0.5 : geometry.size.width * -1)
             .animation(.easeInOut, value: self.showSide)
+
         }
     }
 }
