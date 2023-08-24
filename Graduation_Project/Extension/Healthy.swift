996b4c7b52f9841824d8ea01ca3fc0397b60af8d
Valerie Chen <vchenin620@gmail.com>
2023-08-24 14:18:17 +0800

    test

---
+++ Graduation_Project/Extension/Healthy.swift
@@ -0,0 +1,23 @@
+//建在Extantion
+//  _Member.swift
+//  TopicIM110
+//
+//  Created by on 2023/8/18.
+//
+
+import SwiftUI
+
+extension HealthyView
+{
+    func HealthyButton(title: String) -> some View
+    {
+        Text(title)
+            .font(.body)
+            .foregroundColor(.black)
+            .padding()
+            .frame(width: 300)
+            .background(Color(.systemGray3))
+            .cornerRadius(10)
+            .shadow(color: .gray, radius: 1, y: 3)
+    }
+}
---
+++ Graduation_Project/View/HealthyView.swift
@@ -0,0 +1,78 @@
+//
+//  HealthyView.swift
+//  Graduation_Project
+//
+//  Created by Mac on 2023/8/24.
+//
+
+import SwiftUI
+
+struct HealthyView: View {
+    var body: some View {
+        VStack(spacing:10)
+        {
+            //用戶頭像
+            HStack
+            {
+                Spacer()
+                Circle()
+                    .foregroundColor(.gray)
+                    .frame(width: 150,height: 150)
+                Spacer()
+            }
+
+            //用戶名稱
+            Spacer()
+            Text("秋雨")
+            Spacer()
+
+            VStack
+            {
+                //MARK: 功能按鈕
+                Button
+                {
+
+                }
+            label:
+                {
+                    HealthyButton(title: "BMI")
+                }
+                Spacer()
+                Button
+                {
+
+                }
+            label:
+                {
+                    HealthyButton(title: "卡路里")
+                }
+                Spacer()
+                Button
+                {
+
+                }
+            label:
+                {
+                    HealthyButton(title: "身體素質")
+                }
+                Spacer()
+                Button
+                {
+
+                }
+            label:
+                {
+                    HealthyButton(title: "未來展望")
+                }
+                Spacer()
+
+            }
+        }
+    }
+}
+
+struct HealthyView_Previews: PreviewProvider {
+    static var previews: some View {
+        HealthyView()
+    }
+}
