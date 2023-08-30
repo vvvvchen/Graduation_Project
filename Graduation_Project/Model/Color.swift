2524027521e024f0545cd53e1a33f40b7038f087
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-08-30 11:20:22 +0800

    0830

--- Graduation_Project/Model/Color.swift
+++ Graduation_Project/Model/Color.swift
@@ -1,54 +1,54 @@
-//  _Color.swift
-//  Graduation_Project
+////  _Color.swift
+////  Graduation_Project
+////
+////  Created by Mac on 2023/8/23.
+////
 //
-//  Created by Mac on 2023/8/23.
+//import Foundation
+//import SwiftUI
 //
-
-import Foundation
-import SwiftUI
-
-struct CustomToogleStyle: ToggleStyle
-{
-    func makeBody(configuration: Configuration) -> some View
-    {
-            ZStack
-            {
-                RoundedRectangle(cornerRadius: 20,style: .continuous).frame(width: 150, height: 40)
-                    .foregroundColor(configuration.isOn ? .black:.white)
-
-
-                RoundedRectangle(cornerRadius: 20,style: .continuous)
-                    .foregroundColor(configuration.isOn ? .black:.white)
-                    .animation(.easeInOut, value: configuration.isOn)
-                    .frame(width: 92, height: 44)
-                    .offset(x:configuration.isOn ? 30:-50)
-                    .shadow(color: .black.opacity(0.2), radius: 10, x: 10, y: 10)
-
-
-                HStack
-                {
-                    Text(" 淺色模式")
-                        .bold()
-                        .font(.system(size: 15))
-                        .foregroundColor(configuration.isOn ? .gray:.white)
-
-                    Spacer()
-
-                    Text("深色模式")
-                        .bold()
-                        .font(.system(size: 15))
-                        .foregroundColor(configuration.isOn ? .black:.gray)
-
-                }
-                .frame(width: 135)
-            }
-
-            .onTapGesture
-            {
-                withAnimation(.easeInOut(duration: 0.2))
-                {
-                    configuration.isOn.toggle()
-                }
-            }
-        }
-    }
+//struct CustomToogleStyle: ToggleStyle
+//{
+//    func makeBody(configuration: Configuration) -> some View
+//    {
+//            ZStack
+//            {
+//                RoundedRectangle(cornerRadius: 20,style: .continuous).frame(width: 150, height: 40)
+//                    .foregroundColor(configuration.isOn ? .black:.white)
+//
+//
+//                RoundedRectangle(cornerRadius: 20,style: .continuous)
+//                    .foregroundColor(configuration.isOn ? .black:.white)
+//                    .animation(.easeInOut, value: configuration.isOn)
+//                    .frame(width: 92, height: 44)
+//                    .offset(x:configuration.isOn ? 30:-50)
+//                    .shadow(color: .black.opacity(0.2), radius: 10, x: 10, y: 10)
+//
+//
+//                HStack
+//                {
+//                    Text(" 淺色模式")
+//                        .bold()
+//                        .font(.system(size: 15))
+//                        .foregroundColor(configuration.isOn ? .gray:.white)
+//
+//                    Spacer()
+//
+//                    Text("深色模式")
+//                        .bold()
+//                        .font(.system(size: 15))
+//                        .foregroundColor(configuration.isOn ? .black:.gray)
+//
+//                }
+//                .frame(width: 135)
+//            }
+//
+//            .onTapGesture
+//            {
+//                withAnimation(.easeInOut(duration: 0.2))
+//                {
+//                    configuration.isOn.toggle()
+//                }
+//            }
+//        }
+//    }
