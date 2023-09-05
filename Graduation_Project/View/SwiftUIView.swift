9a9d44334c8bf7a8efaeb2e8bcee4b1d91c3ae34
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-09-05 18:05:34 +0800

    0905 fin

--- Graduation_Project/View/SwiftUIView.swift
+++ Graduation_Project/View/SwiftUIView.swift
@@ -1,138 +1,122 @@
-//
-//  SwiftUIView.swift
-//  Graduation_Project
-//
-//  Created by Mac on 2023/8/24.
-//
-
 import SwiftUI
 
 struct SwiftUIView: View
 {
-    //是否顯示底部的彈出式圖
-    @State private var showSheet: Bool=false
-    //控制彈出式圖是否在頂部
-    @State private var up: Bool=false
-    //當前拖動垂直位置
-    @State private var drag: CGFloat=0
-    //最終位置（上方或下方）用於隱藏或顯示彈出式視圖
-    @State private var end: CGFloat=0
-    //起始位置，用於恢復視圖位置
-    @State private var start: CGFloat=0
-    //存儲被點擊按鈕的標題
-    @State private var selectedTitle: String = ""
-    
-    
-    //標題陣列
-    private let title: [String]=["番茄炒蛋", "荷包蛋", "炒高麗菜", "冰糖肘子"]
-    private let englishtitle: [String]=["Tomato Scrambled Eggs", "Poached Eggs", "Fried Cabbage", "Pork Joint Stewed With Rock Sugar"]
-    
-    
-    var body: some View
-    {
-        NavigationStack
-        {
+   //设置最小高度和最大高度
+   private let minHeight: CGFloat = 60
+   private let maxHeight: CGFloat = 200
+   
+   //用於控制是否顯示標題
+   @State private var showTitle: Bool = false
+   //用於控制是否顯示彈出式視圖
+   @State private var showSheet: Bool = false
+   //用於存儲用戶選擇的標題
+   @State private var selectedTitle: String = ""
+   
+   
+   //標題陣列
+   private let title: [String] = ["番茄炒蛋", "荷包蛋", "炒高麗菜", "冰糖肘子"]
+   
+   var body: some View
+   {
+      NavigationStack
+      {
+         ScrollView(.vertical, showsIndicators: false)
+         {
             VStack(spacing: 50)
             {
-                //遍歷標題數組，為每個標題創建按鈕
-                ForEach(self.title, id: \.self)
-                {
-                    index in
-                    Button(action:
-                            {
-                        //點擊按鈕時顯示彈出視圖，並將選定的標題存儲到selectedTitle
-                        self.selectedTitle = index
-                        //點擊按鈕時顯示或隱藏彈出式視圖
-                        self.showSheet.toggle()
-                    }){
-                        Text(index)
-                            .bold()
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .frame(width: 300)
-                            .background(.black)
-                            .cornerRadius(10)
-                    }
-                }
+               //遍歷標題數組，為每個標題創建按鈕
+               ForEach(self.title, id: \.self)
+               {
+                  index in
+                  Button(action:
+                           {
+                     //點擊按鈕時更新選定的標題和顯示sheet的狀態
+                     self.selectedTitle = index
+                     self.showSheet = true
+                  }) {
+                     Text(index)
+                        .bold()
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .frame(width: 300)
+                        .background(.black)
+                        .cornerRadius(10)
+                  }
+               }
             }
-            //彈出式視圖
-            .sheet(isPresented: self.$showSheet)
+         }
+         
+         //MARK: 彈出式視圖
+         .sheet(isPresented: self.$showSheet)
+         {
+            NavigationStack
             {
-                NavigationStack
-                {
-                    ZStack(alignment: .top)
-                    {
-                        //背景色
-                        Color(.systemGray6).ignoresSafeArea()
+               ScrollView
+               {
+                  LazyVStack
+                  {
+                     ZStack
+                     {
+                        Rectangle()
+                           .frame(width: 500, height: maxHeight)
+                           .foregroundColor(.blue)
                         
-                        VStack(spacing: 0)
+                        Text(selectedTitle)
+                           .bold()
+                           .font(.system(size: 28))
+                     }
+                     
+                     //用於生成數字 1 到 20，個循環將遍歷這些數字並根據條件分別創建不同的視圖。
+                     ForEach(1..<21)
+                     {
+                        index in
+                        //如果內容小於等於11
+                        if (index <= 11)
                         {
-                            Rectangle()
-                                .fill(.indigo.gradient)
-                            //頭部顏色區域
-                                .frame(height: 300)
-                            
-                            VStack
-                            {
-                                Text(selectedTitle)
-                                    .bold()
-                                    .font(.largeTitle)
-                                
-                                Text(englishtitle[title.firstIndex(of: selectedTitle) ?? 0])
-                                    .font(.headline)
-                                    .foregroundColor(.gray)
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .offset(y: self.start)
-                            .offset(y: self.drag)
-                            .offset(y: self.end)
-                            //拖動手勢處理
-                            .gesture(
-                                DragGesture()
-                                    .onChanged {value in
-                                        withAnimation(.easeInOut)
-                                        {
-                                            self.drag=value.translation.height
-                                        }
-                                    }
-                                    .onEnded
-                                {
-                                    value in
-                                    withAnimation(.spring())
-                                    {
-                                        //根據拖動距離決定最終位置
-                                        self.end=self.drag <= -100 ? -150:0
-                                        self.drag=0
-                                    }
-                                }
-                            )
-                            //MARK: 測試文本
-                            ScrollView(.vertical, showsIndicators: false) {
-                                Text("This is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.\nThis is an additional text.")
-                                    .font(.system(size:20))
-                                    .foregroundColor(.black)
-                                    .padding(.bottom, 20)
-                            }
+                           //顯示
+                           Text("\(index)")
+                              .font(.title)
+                              .padding(.vertical)
+                              .frame(maxWidth: .infinity)
                         }
-                    }
-                    //導航欄標題
-                    .navigationBarTitleDisplayMode(.inline)
-                }
-                //彈出視圖内容交互方式
-                .presentationContentInteraction(.scrolls)
-                .presentationCornerRadius(20)
-                //彈出視圖拖動指示器
-                .presentationDragIndicator(.visible)
+                        //如果內容大於12
+                        else
+                        {
+                           Text("\(index)")
+                              .font(.title)
+                              .padding(.vertical)
+                              .frame(maxWidth: .infinity)
+                              .onAppear {
+                                 withAnimation(.easeInOut)
+                                 {
+                                    self.showTitle = true
+                                 }
+                              }
+                              .onDisappear {
+                                 withAnimation(.easeInOut)
+                                 {
+                                    self.showTitle = false
+                                 }
+                              }
+                        }
+                     }
+                  }
+                  .background(Color(.systemGray6))
+               }
+               .background(Color(.systemGray2))
+               .navigationTitle(self.showTitle ? selectedTitle : "")
+               .navigationBarTitleDisplayMode(.inline)
             }
-        }
-    }
+         }
+      }
+   }
 }
 struct SwiftUIView_Previews: PreviewProvider
 {
-    static var previews: some View
-    {
-        SwiftUIView()
-    }
+   static var previews: some View
+   {
+      SwiftUIView()
+   }
 }
