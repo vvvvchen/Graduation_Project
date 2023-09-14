06860f8fb14b6387f5526f655e389ca45ba2f17c
yanyanyanling <s11014069@gm.cyut.edu.tw>
2023-09-14 17:39:42 +0800

    0914 PM.5:39

--- Graduation_Project/View/ContentView.swift
+++ Graduation_Project/View/ContentView.swift
@@ -1,82 +1,133 @@
 //
 //  ContentView.swift
 //
-//  Created by 0822
+//  Created by 0914
 //
 
 import SwiftUI
 
-struct ContentView: View
-{
+struct ContentView: View {
+    // Circle大小
+    let circleSize: CGFloat = 50
+    // 設定發文每行可輸入的字數
+    let maxCharactersPerLine = 16
+    // 預設輸入時沒有東西
+    @State private var userInput: String = ""
+    // 控制點按加號後會出現的Zstack
+    @State private var isExpanded = false
     
     // 切換深淺模式
     @State var isDarkMode: Bool = false
+    
     // TabView選擇的頁面
     @State private var showSide: Bool = false
     // 跟蹤標籤頁
     @State private var select: Int = 0
-    // 會員預設資料
     @State private var information: Information = Information(name: "vc", gender: "女性", age: 20, height: 161, weight: 50, BMI: 19.68)
     // 展開搜索列設為FALSE
     @State private var isSearching = false
-    // 創建相機所需的狀態管理對象
+    
     @StateObject private var cameraManagerViewModel = CameraManagerViewModel()
     
     var body: some View
     {
         HStack
         {
-            // MARK: 主頁和分享
-            if self.select == 0 || self.select == 1
+            // MARK: 主頁
+            if self.select == 0
             {
                 ZStack
                 {
-                    VStack(spacing: 0)
+                    // 應該會包住篩選和食譜
+                    VStack
                     {
-                        HStack
+                        // 第一個視圖
+                        VStack(spacing: 0)
                         {
-                            Button(action:
+                            HStack
+                            {
+                                Button(action:
+                                        {
+                                    // 展開搜索列設為TRUE
+                                    isSearching = true
+                                }) {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .fill(Color(red: 0.961, green: 0.804, blue: 0.576))
+                                        .frame(width: 300, height: 35, alignment: .leading)
+                                        .overlay
                                     {
-                                // 展開搜索列設為TRUE
-                                isSearching = true
-                            }) {
-                                RoundedRectangle(cornerRadius: 5)
-                                    .fill(Color(red: 0.961, green: 0.804, blue: 0.576))
-                                    .frame(width: 300, height: 35, alignment: .leading)
-                                    .overlay
-                                {
-                                    Image(systemName: "magnifyingglass")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundColor(.white)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-                                        .frame(height: 20)
-                                        .padding(.horizontal, 10)
+                                        Image(systemName: "magnifyingglass")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.white)
+                                            .frame(maxWidth: .infinity, alignment: .trailing)
+                                            .frame(height: 20)
+                                            .padding(.horizontal, 10)
+                                    }
                                 }
+                                
                             }
-                            if self.select == 0
-                            {
-                                Image(systemName: "list.bullet")
-                                    .font(.system(size: 30))
-                                    .foregroundColor(.white)
-                            }
-                            else if self.select == 1
-                            {
-                                Image(systemName: "plus")
-                                    .font(.system(size: 35))
-                                    .foregroundColor(.white)
-                            }
+                            .padding(10)
                         }
-                        .padding(10)
+                        // MARK: 最上方搜尋列和清單或加號的背景調整
+                        .frame(width:400)
+                        .background(Color(.systemOrange))
+                        //                .background(Color(.orange))
+                        .padding(.bottom, -10)
+                        .fullScreenCover(isPresented: $isSearching)
+                        {
+                            SearchView(isSearching: $isSearching)
+                        }
                     }
                 }
-                // MARK: 最上方搜尋列和清單或加號的背景調整
-                .frame(width:400)
-                .background(Color(.systemOrange))
-                //                .background(Color(.orange))
-                .padding(.bottom, -10)
             }
-            // MARK: 會員
+            // MARK: 分享
+            if self.select == 1
+            {
+                ZStack
+                {
+                    // 包住加號和發佈貼文
+                    VStack
+                    {
+                        VStack(spacing: 0)
+                        {
+                            // 第一個視圖
+                            HStack
+                            {
+                                Button(action:
+                                        {
+                                    // 展開搜索列設為TRUE
+                                    isSearching = true
+                                }) {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .fill(Color(red: 0.961, green: 0.804, blue: 0.576))
+                                        .frame(width: 300, height: 35, alignment: .leading)
+                                        .overlay
+                                    {
+                                        Image(systemName: "magnifyingglass")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(.white)
+                                            .frame(maxWidth: .infinity, alignment: .trailing)
+                                            .frame(height: 20)
+                                            .padding(.horizontal, 10)
+                                    }
+                                }
+                            }
+                            .padding(10)
+                        }
+                        // MARK: 最上方搜尋列和清單或加號的背景調整
+                        .frame(width:400)
+                        .background(Color(.systemOrange))
+                        //                .background(Color(.orange))
+                        .padding(.bottom, -10)
+                        .fullScreenCover(isPresented: $isSearching)
+                        {
+                            SearchView(isSearching: $isSearching)
+                        }
+                    }
+                }
+            }
             else if self.select == 4
             {
                 HStack
@@ -92,40 +143,40 @@ struct ContentView: View
             }
             Spacer()
         }
-        .fullScreenCover(isPresented: $isSearching)
-        {
-            SearchView(isSearching: $isSearching)
-        }
+        // MARK: TabView
         ZStack
         {
-            // MARK: TabView
             TabView(selection: self.$select)
             {
-                //              HomeView(select: self.$select)
+                //                  HomeView(select: self.$select)
                 HomeView()
                     .tag(0)
                     .tabItem
                 {
                     Label("主頁", systemImage: "house.fill")
                 }
+                
                 ForumView()
                     .tag(1)
                     .tabItem
                 {
                     Label("分享", systemImage: "globe")
                 }
+                
                 CameraContentView(cameraManagerViewModel: self.cameraManagerViewModel)
                     .tag(2)
                     .tabItem
                 {
                     Label("AI", systemImage: "camera")
                 }
+                
                 FavoriteView(select: self.$select)
                     .tag(3)
                     .tabItem
                 {
                     Label("最愛", systemImage: "heart.fill")
                 }
+                
                 MyView(select: self.$select, information: self.$information)
                     .tag(4)
                     .tabItem
--- Graduation_Project/View/ForumView.swift
+++ Graduation_Project/View/ForumView.swift
@@ -2,19 +2,90 @@
 //  ForumView.swift
 //  Graduation_Project
 //
-//  Created by Mac on 2023/9/7.
+//  Created by Mac on 2023/9/14.
 //
 
 import SwiftUI
 
-struct ForumView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ForumView: View
+{
+    @State private var isSheetPresented = false
+    @State private var buttonOffset: CGSize = .zero
+    @State private var isDragging = false
+    @State private var buttonPosition: CGPoint = CGPoint(x: 350, y: 600)
+
+    var body: some View
+    {
+        ZStack
+        {
+            // MARK: 使用者發布文章區塊
+            Button(action:
+                    {
+                isSheetPresented = true
+            }) {
+                ZStack
+                {
+                    Circle()
+                        .fill(Color(.systemOrange))
+                        .frame(width: 80, height: 80)
+                        .offset(buttonOffset)
+                    
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width: 35, height: 35)
+                        .foregroundColor(.white)
+                        .offset(buttonOffset)
+                }
+            }
+            .offset(buttonOffset)
+
+            // MARK: 發布文章按鈕
+
+
+        }
+        .sheet(isPresented: $isSheetPresented)
+        {
+            Text("This is a sheet.")
+        }
+        
+        .gesture(
+            // 手勢類型，會在用戶開始拖動時觸發
+            DragGesture()
+                // 用戶拖動發生變化時，會執行括號中的代碼
+                .onChanged
+            {
+                value in
+                    // 把按鈕偏移位置設置為當前拖動的位移
+                    buttonOffset = value.translation
+                    // 按鈕正在被拖動
+                    isDragging = true
+                }
+                .onEnded
+            {
+                value in
+                    // 按鈕停止拖動
+                    isDragging = false
+                    // 更新按鈕位置(限制按鈕可以移動到哪裡)
+                    let newX = min(max(45, buttonPosition.x + value.translation.width), 350)
+                    let newY = min(max(50, buttonPosition.y + value.translation.height), 600)
+
+                    withAnimation
+                {
+                        buttonPosition = CGPoint(x: newX, y: newY)
+                    }
+                    // 重置按鈕偏移
+                    buttonOffset = .zero
+                }
+        )
+        // 用保存的位置來設置按鈕位置
+        .position(buttonPosition)
     }
 }
 
-struct ForumView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ForumView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ForumView()
     }
 }
